8fbcd81391d3bdf4e211ac6a10f43eb97b81d9df
pesl98 <142811939+pesl98@users.noreply.github.com>
2024-02-23 08:02:53 +0000

    test

--- Hello.py
+++ Hello.py
@@ -1,22 +1,7 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
-
 
 def run():
     st.set_page_config(
@@ -30,7 +15,7 @@ def run():
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
+        Streamlit is an app framework built specifically for
         Machine Learning and Data Science projects.
         **👈 Select a demo from the sidebar** to see some examples
         of what Streamlit can do!
